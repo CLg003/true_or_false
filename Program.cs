8707e1111eb4d6c2d63aeb0794b51e99acb66288
Claire <97535035+CLg003@users.noreply.github.com>
2022-07-19 15:33:46 +0100

    Set up.

--- Program.cs
+++ Program.cs
@@ -12,7 +12,25 @@ namespace TrueOrFalse
             Tools.SetUpInputStream(entry);
 
             // Type your code below
-        
+            string[] questions = {
+                "Is blue the same colour as red?",
+                "Is your name Mary Poppins?",
+                "Does a rainbow have 9 colours?"
+                "Does 2 + 3 = 5?"
+            }
+
+            bool[] answers = {
+                false,
+                false,
+                false,
+                true
+            }
+
+            bool[] responses = new bool[questions.Length];
+
+            if (questions.Length != answers.Length) {
+                Console.WriteLine("Warning: The number of answers is not the same as the number of questions!");
+            }
         }
     }
 }
